31f36c114763520b961bca139d808fa718af1787
Marvellegend <amalpanil06@gmail.com>
2019-03-12 10:52:31 +0530

    done

---
+++ Addprm.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Addpgm{
+    public static void main(String[] args) {
+        Scanner myinput=new Scanner(System.in);
+        int a,b;
+        System.out.println("Enter the first number :");
+        a=myinput.nextInt();
+        System.out.println("Enter the second number :");
+        b=myinput.nextInt();
+        a=a+b;
+        System.out.println("sum is:"+a);
+    }
+}
